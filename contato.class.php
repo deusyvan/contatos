d3db348cb9b6202737e0374d91cfe236774025fe
2º Sgt Deusyvan <deusyvan.silva@eb.mil.br>
2018-07-25 10:44:42 -0300

    Criando nossa classe contato com acesso ao banco de dados

---
+++ contato.class.php
@@ -0,0 +1,12 @@
+<?php
+class Contato {
+    //privada para poder ser usado em toda a classe
+    private $pdo;
+    
+    // para facilitar conexao aqui dentro do construtor
+    public function __construct(){
+        
+        //acessando na propria classe this
+        $this->pdo = new PDO("mysql:dbname=projeto_contatos;host=localhost","root","root");
+    }
+}
