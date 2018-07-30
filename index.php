4f9761e6df601e829cfe0cb8cef961c5ff22820d
2º Sgt Deusyvan <deusyvan.silva@eb.mil.br>
2018-07-30 16:53:55 -0300

    Inclusão de arquivos para o template

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
-<?php 
-include 'contato.class.php';
-
-$contato = new Contato();
-
+<html>
+<head>
+	<title>Contatos</title>
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="assets/css/style.css" />
+	<script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
+	<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="assets/js/script.js"></script>
+</head>
+<body>
+	<nav class="navbar navbar-inverse">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a href="./" class="navbar-brand">Contatos</a>
+			</div>
+		</div>
+	</nav>
+</body>
+</html>
