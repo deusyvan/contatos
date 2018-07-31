c4bd01a58ea6733ffcafaba01cc5f84b5076bdbe
2º Sgt Deusyvan <deusyvan.silva@eb.mil.br>
2018-07-31 11:30:12 -0300

    Realiza o cadastro usando OO e ainda fazendo algumas validações e enviando mensagens de informação

--- cadastre-se.php
+++ cadastre-se.php
@@ -10,17 +10,30 @@
 	       $senha = $_POST['senha'];
 	       $telefone = addslashes($_POST['telefone']);
 	       
-	       if (!empty($nome) && !empty($email) && !empty($senha)){
-	           $u->cadastrar($nome, $email,$senha, $telefone);
+	       if(!empty($nome) && !empty($email) && !empty($senha)){
+	           if($u->cadastrar($nome, $email, $senha, $telefone)){
+	               ?>
+                    <div class="alert alert-success">
+                    	<strong>Parabéns! </strong> Cadastrado com sucesso. 
+                    	<a href="login.php" class="alert-link">Faça o login agora</a>
+                    </div>
+                    <?php 
 	       } else {
 	           ?>
-	           <div class="alert alert-warning">
-	           	Preencha todos os campos
-	           </div>
-	           <?php 
-	       }
+                <div class="alert alert-warning">
+                	Este usuário já existe 
+                	<a href="login.php" class="alert-link">Faça o login agora</a>
+                </div>
+                <?php 
+                }
+            } else {
+                ?>
+                <div class="alert alert-warning"> Preencha todos os campos</div>
+                <?php 
+            }
 	   }
-	?>
+    ?>
+
 	<form method="POST">
 		<div class="form-group">
 			<label for="nome">Nome: </label> 
@@ -42,3 +55,4 @@
 	</form>
 </div>
 <?php require 'pages/footer.php'; ?>
+
--- config.php
+++ config.php
@@ -1,9 +1,11 @@
 <?php 
 session_start();
 
+global $pdo;
+
 try {
     
-    $db = new PDO("mysql:dbname=projeto_contatos;host=localhost", "admin", "admin@12");
+    $pdo = new PDO("mysql:dbname=projeto_contatos;host=localhost", "admin", "admin@12");
     
 } catch ( PDOException $e) {
     echo "ERRO: ".$e->getMessage();
