6b380e97a5e7463c343ce2a90b6c997034111f2e
deusyvan <deusyvan@gmail.com>
2018-07-30 23:47:56 -0300

    Começando nossa estrutura do projeto

--- index.php
+++ index.php
@@ -13,7 +13,27 @@
 			<div class="navbar-header">
 				<a href="./" class="navbar-brand">Contatos</a>
 			</div>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="">Cadastre-se</a></li>
+				<li><a href="">Login</a></li>
+			</ul>
 		</div>
 	</nav>
+	
+	<div class="container-fluid">
+		<div class="jumbotron">
+			<h2>Nós temos hoje 20 mensagens</h2>
+			<p>E mais de 300 usuários cadastrados</p>
+		</div>
+		
+		<div class="row">
+			<div class="col-sm-3">
+				<h4>Pesquisa Avançada</h4>
+			</div>
+			<div class="col-sm-9">
+				<h4>Últimas mensagens</h4>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
