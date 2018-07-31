03528e0cf0761e5001a857b4dcd394cf02682148
2º Sgt Deusyvan <deusyvan.silva@eb.mil.br>
2018-07-31 12:53:59 -0300

    Validando o usuario no login e jogando na sessão

--- classes/usuarios.class.php
+++ classes/usuarios.class.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 class Usuarios {
     
     public function cadastrar($nome, $email,$senha, $telefone){
@@ -23,5 +23,22 @@ class Usuarios {
         }
     }
     
+    public function login ($email, $senha){
+        global $pdo;
+        
+        $sql = $pdo->prepare("SELECT id FROM usuarios WHERE email = :email AND senha = :senha");
+        $sql->bindValue(":email", $email);
+        $sql->bindValue(":senha", md5($senha));
+        $sql->execute();
+        
+        if($sql->rowCount() > 0){
+            $dado = $sql->fetch();
+            $_SESSION['cLogin'] = $dado['id'];
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
 }
+
 ?>
